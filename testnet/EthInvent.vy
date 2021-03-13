2c85d75ccda750eee1940fe1eed44a60cda4a8ae
ausernamewastaken <80340682+ausernamewastaken@users.noreply.github.com>
2021-03-13 05:58:14 +0000

    updated code

--- testnet/EthInvent.vy
+++ testnet/EthInvent.vy
@@ -1,20 +1,17 @@
 # @author ausernamewastaken
 
-currentSubmissionCount: public(uint256)
-countToSubmission: HashMap[uint256, bytes32]
+submissions: public(HashMap[bytes32, uint256])
 
 event HashSubmitted:
-    submissionCount: uint256
     hashOfContent: bytes32
 
 @external
-def __init__():
-    self.currentSubmissionCount = 0
-
-@external
 def submitHash(_hashOfContent: bytes32) -> bool:
-    self.countToSubmission[self.currentSubmissionCount] = _hashOfContent
-    log HashSubmitted(self.currentSubmissionCount, _hashOfContent)
+
+    assert self.submissions[_hashOfContent] == 0
+
+    self.submissions[_hashOfContent] = block.timestamp
+
+    log HashSubmitted(_hashOfContent)
     
-    self.currentSubmissionCount += 1
     return True
--- testnet/EthInvent_Client.py
+++ testnet/EthInvent_Client.py
@@ -1,18 +1,18 @@
 # @author ausernamewastaken
 
 import hashlib
-import hexbytes
+from hexbytes.main import HexBytes
 from web3 import Web3
 
 
 web3_endpoint = 'https://ropsten.infura.io/v3/211f7bc921be4ccdac857047379b3a23'
 
-contract_address = '0x9067fefd6a12ddd3f15fb58448fd701baa9a39ad'
+contract_address = '0xFcc69d39377E2190Fe72FBc84FE234cfB783Ae46'
 
 
 abi = \
     """
-    [{"name":"HashSubmitted","inputs":[{"type":"uint256","name":"submissionCount","indexed":false},{"type":"bytes32","name":"hashOfContent","indexed":false}],"anonymous":false,"type":"event"},{"outputs":[],"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"name":"submitHash","outputs":[{"type":"bool","name":""}],"inputs":[{"type":"bytes32","name":"_hashOfContent"}],"stateMutability":"nonpayable","type":"function","gas":74914},{"name":"currentSubmissionCount","outputs":[{"type":"uint256","name":""}],"inputs":[],"stateMutability":"view","type":"function","gas":1091}]
+    [{"name": "HashSubmitted", "inputs": [{"type": "bytes32", "name": "hashOfContent", "indexed": false}], "anonymous": false, "type": "event"}, {"name": "submitHash", "outputs": [{"type": "bool", "name": ""}], "inputs": [{"type": "bytes32", "name": "_hashOfContent"}], "stateMutability": "nonpayable", "type": "function", "gas": 37744}, {"name": "submissions", "outputs": [{"type": "bytes32", "name": ""}], "inputs": [{"type": "bytes32", "name": "arg0"}], "stateMutability": "view", "type": "function", "gas": 1206}]
     """
 
 
@@ -20,7 +20,7 @@ w3 = Web3(Web3.HTTPProvider(web3_endpoint))
 contract = w3.eth.contract(address=contract_address, abi=abi)
 
 
-def submit_invention(text_description: bytes, wallet: hexbytes.main.HexBytes, gas: int = None) -> bool:
+def submit_invention(text_description: bytes, wallet: HexBytes, gas: int = None) -> HexBytes:
     """
     Submits an invention by writing its SHA3_256 hash to the blockchain
     """
@@ -45,7 +45,24 @@ def submit_invention(text_description: bytes, wallet: hexbytes.main.HexBytes, ga
     return txhash
 
     
+def verify_proof(text_description: bytes, return_timestamp: bool = False) -> (bool or int):
+    """
+    Verifies a proof by checking its SHA3_256 hash aginst the record on blockchain
+    
+    Returns:
+        True if the proof is sucessfully verified and return_timestamp is False;
+        The timestamp at which the hash was written onto the blockchain if the proof is sucessfully verified
+        and return_timestamp is True;
+    """
 
+    _hashOfContent = hashlib.sha3_256(text_description).digest()
+    
+    recorded_timestamp = contract.functions.submissions(_hashOfContent).call()
+
+    return recorded_timestamp if return_timestamp else (recorded_timestamp == 0)
+
+    
+    
 if __name__ == '__main__':
     import json
     
